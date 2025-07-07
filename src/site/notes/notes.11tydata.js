989ad486050dd46d58d46fd06df6e7fb38d22907
Zhonghao Guo <32609824+h4rvey-g@users.noreply.github.com>
2025-07-07 12:08:30 +0000

    fix chinese

--- src/site/notes/notes.11tydata.js
+++ src/site/notes/notes.11tydata.js
@@ -1,33 +1,21 @@
-require("dotenv").config();
-const settings = require("../../helpers/constants");
-
-const allSettings = settings.ALL_NOTE_SETTINGS;
-
 module.exports = {
   eleventyComputed: {
-    layout: (data) => {
-      if (data.tags.indexOf("gardenEntry") != -1) {
-        return "layouts/index.njk";
-      }
-      return "layouts/note.njk";
-    },
     permalink: (data) => {
-      if (data.tags.indexOf("gardenEntry") != -1) {
-        return "/";
-      }
-      return data.permalink || undefined;
-    },
-    settings: (data) => {
-      const noteSettings = {};
-      allSettings.forEach((setting) => {
-        let noteSetting = data[setting];
-        let globalSetting = process.env[setting];
+      if (data.permalink) {
+        // The permalink might be an object (from a directory data file) or a string (from front matter)
+        const permalinkValue = typeof data.permalink === 'object' ? data.permalink.toString() : data.permalink;
 
-        let settingValue =
-          noteSetting || (globalSetting === "true" && noteSetting !== false);
-        noteSettings[setting] = settingValue;
-      });
-      return noteSettings;
+        if (typeof permalinkValue === 'string') {
+            // Split the path into segments, filtering out empty strings from leading/trailing slashes
+            const parts = permalinkValue.split('/').filter(part => part);
+            // Encode each part and join them back with slashes
+            const encodedPermalink = parts.map(part => encodeURIComponent(part)).join('/');
+            // Add leading and trailing slashes back
+            return `/${encodedPermalink}/`;
+        }
+      }
+      // If no permalink is set in the front matter, return undefined to let Eleventy use its default logic
+      return undefined;
     },
   },
 };
